a92c32b57716c35831d7fed102238745f3b106dd
Sid Med <97449309+sid-hbm@users.noreply.github.com>
2026-01-10 08:53:34 -0800

    docker compose

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -91,6 +91,18 @@
 /******/ 		};
 /******/ 	}();
 /******/ 	
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	!function() {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = function(module) {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				function() { return module['default']; } :
+/******/ 				function() { return module; };
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	}();
+/******/ 	
 /******/ 	/* webpack/runtime/create fake namespace object */
 /******/ 	!function() {
 /******/ 		var getProto = Object.getPrototypeOf ? function(obj) { return Object.getPrototypeOf(obj); } : function(obj) { return obj.__proto__; };
@@ -180,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d0b3b79809a1043c"; }
+/******/ 		__webpack_require__.h = function() { return "27ae6efe8b6eb931"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
